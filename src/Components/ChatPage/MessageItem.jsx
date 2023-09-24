8945877dea58b66f4380c03e2672865d4c91a492
Kalinga <kalingakhatua@gmail.com>
2023-09-24 15:53:18 +0530

    basic gap clamps

--- src/Components/ChatPage/MessageItem.jsx
+++ src/Components/ChatPage/MessageItem.jsx
@@ -9,7 +9,8 @@ export default function MessageItem({ user, text, isUser }) {
 			sx={{
 				display: "flex",
 				flexDirection: isUser ? "row-reverse" : "",
-				gap: 2,
+				gap: "clamp(0.5rem, 3vw, 1rem)",
+				alignItems: "flex-start",
 				backgroundColor: isUser && "var(--accent-color-20)",
 				borderRadius: 2,
 			}}
@@ -19,9 +20,12 @@ export default function MessageItem({ user, text, isUser }) {
 					alt="Profile Pic"
 					src={
 						isUser
-							? "https://c.ndtvimg.com/2022-05/mbbokrb8_disha_625x300_12_May_22.jpg"
+							? "https://wallpapers.com/images/hd/cute-girl-vector-art-profile-picture-jhbu3wt713zj2bti.jpg"
 							: "https://wallpapers.com/images/hd/cute-girl-vector-art-profile-picture-jhbu3wt713zj2bti.jpg"
 					}
+					sx={{
+						marginBlock: "0.4rem",
+					}}
 				/>
 			</ListItemIcon>
 			<ListItemText

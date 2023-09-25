f970c68759df83c619ed637962379dbf3406cec2
Sarans13 <114057996+Sarans13@users.noreply.github.com>
2023-09-25 12:30:00 +0530

    nav link updated

--- src/Components/LandingPage/NavBar.jsx
+++ src/Components/LandingPage/NavBar.jsx
@@ -5,76 +5,73 @@ import { Link } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 
 export const NavBar = () => {
-	const [activeLink, setActiveLink] = useState("home");
-	const [scrolled, setScrolled] = useState(false);
+  const [activeLink, setActiveLink] = useState("home");
+  const [scrolled, setScrolled] = useState(false);
 
-	useEffect(() => {
-		const onScroll = () => {
-			if (window.scrollY > 50) {
-				setScrolled(true);
-			} else {
-				setScrolled(false);
-			}
-		};
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
 
-		window.addEventListener("scroll", onScroll);
+    window.addEventListener("scroll", onScroll);
 
-		return () => window.removeEventListener("scroll", onScroll);
-	}, []);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
 
-	const onUpdateActiveLink = (value) => {
-		setActiveLink(value);
-	};
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  };
 
-	return (
-		<Navbar
-			expand="md"
-			className={scrolled ? "scrolled" : ""}
-		>
-			<Container>
-				<Navbar.Brand
-					href="/"
-					className="text-light fs-3"
-				>
-					TaskGuide
-				</Navbar.Brand>
-				<Navbar.Toggle aria-controls="basic-navbar-nav">
-					<span className="navbar-toggler-icon"></span>
-				</Navbar.Toggle>
-				<Navbar.Collapse id="basic-navbar-nav">
-					<Nav className="ms-auto">
-						<Nav.Link
-							href="#home"
-							className={
-								activeLink === "home"
-									? "active navbar-link"
-									: "navbar-link"
-							}
-							onClick={() => onUpdateActiveLink("home")}
-						>
-							Home
-						</Nav.Link>
-						<Nav.Link
-							href="#skills"
-							className={
-								activeLink === "skills"
-									? "active navbar-link"
-									: "navbar-link"
-							}
-							onClick={() => onUpdateActiveLink("skills")}
-						>
-							About us
-						</Nav.Link>
-					</Nav>
-					<span className="navbar-text">
-						<Link to="/chat">
-							<button className="vvd">
-								<span>Try Chatbot</span>
-							</button>
-						</Link>
-					</span>
-				</Navbar.Collapse>
-			</Container>
-		</Navbar>
-	);
+  return (
+    <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
+      <Container>
+        <Navbar.Brand href="/" className="text-light fs-3">
+          TaskGuide
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          <span className="navbar-toggler-icon"></span>
+        </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link
+              href="#home"
+              className={
+                activeLink === "home" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("home")}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              href="#projects"
+              className={
+                activeLink === "projects" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("projects")}
+            >
+              About us
+            </Nav.Link>
+          </Nav>
+          <Nav.Link 
+          href="#projects"
+          className={
+            activeLink === "projects" ? "active navbar-link" : "navbar-link"
+          }
+          onClick={() => onUpdateActiveLink("projects")}>
+          <span className="navbar-text">
+            <button className="vvd" >
+              <span className="Lekha-link">
+                  Try ChatBot
+              </span>
+            </button>
+            </span>
+          </Nav.Link>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
